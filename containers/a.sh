d97dd5a67b719ccd1d535e9863e320cff86f104b
Aaron Wislang <aaron.wislang@microsoft.com>
2018-04-03 17:10:18 -0400

    Moved to: azure-containers/ under github.com/aaronmsft/aaronmsft-com/

--- containers/a.sh
+++
@@ -1,9 +0,0 @@
-# VARIABLES
-# ---------
-RESOURCE_GROUP='180300-demo'
-LOCATION='eastus'
-REGISTRY_NAME='acr180300demo'
-CONTAINER_NAME='aci-1'
-CLUSTER_NAME='aks180300demo'
-WEBAPP_NAME='hello-webapp-180300'
-DIR=$(pwd)
